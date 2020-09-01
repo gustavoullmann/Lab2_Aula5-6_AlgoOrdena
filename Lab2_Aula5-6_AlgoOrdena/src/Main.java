33817c6768f85dccae2d8cfb3c0164168e7d1c1a
gustavo <gustavo@gustavo-Vostro-3460>
2020-09-01 12:12:35 -0300

    Inclusão do medidor de tempo de execução dos médotos Readequação de toda classe Main

--- Lab2_Aula5-6_AlgoOrdena/src/Main.java
+++ Lab2_Aula5-6_AlgoOrdena/src/Main.java
@@ -2,81 +2,154 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int tamanhoArray = 100;
+		int tamanhoArray = 10000;
+		int quantExecucoes = 10;
 		
-		System.out.println("\n" + "Array Shuffle");
-		ArraysTeste arrayShuffle1 = new ArraysTeste(tamanhoArray);
-		arrayShuffle1.fillArrayAscending();
-		arrayShuffle1.randomizeArray();
-		arrayShuffle1.printArray();
-		System.out.println("\n" + "Ordenado Bubble Sort");
-		arrayShuffle1.bubbleSort();
-		arrayShuffle1.printArray();
-		
-		System.out.println("\n" + "Array Shuffle ");
-		ArraysTeste arrayShuffle2 = new ArraysTeste(tamanhoArray);
-		arrayShuffle2.fillArrayAscending();
-		arrayShuffle2.randomizeArray();
-		arrayShuffle2.printArray();
-		System.out.println("\n" + "Ordenado Insert Sort");
-		arrayShuffle2.insertSort();
-		arrayShuffle2.printArray();
+		System.out.println("Arrays com " + tamanhoArray + " elementos");
+				
+			System.out.println("\n\t" + "Array Shuffle");
+			
+				ArraysTeste arrayShuffle1 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Bubble Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+							
+							arrayShuffle1.fillArrayAscending();
+							arrayShuffle1.randomizeArray();
+				
+							long startTime = System.currentTimeMillis();
+							arrayShuffle1.bubbleSort();
+							long endTime = System.currentTimeMillis();
+						
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}
 
-		System.out.println("\n" + "Array Shuffle ");
-		ArraysTeste arrayShuffle3 = new ArraysTeste(tamanhoArray);
-		arrayShuffle3.fillArrayAscending();
-		arrayShuffle3.randomizeArray();
-		arrayShuffle3.printArray();
-		System.out.println("\n" + "Ordenado Selection Sort");
-		arrayShuffle3.selectSort();
-		arrayShuffle3.printArray();
-		
-		System.out.println("\n" + "Array Ascending");
-		ArraysTeste arrayAscending1 = new ArraysTeste(tamanhoArray);
-		arrayAscending1.fillArrayAscending();
-		arrayAscending1.printArray();
-		System.out.println("\n" + "Ordenado Bubble Sort");
-		arrayAscending1.bubbleSort();
-		arrayAscending1.printArray();
-		
-		System.out.println("\n" + "Array Ascending");
-		ArraysTeste arrayAscending2 = new ArraysTeste(tamanhoArray);
-		arrayAscending2.fillArrayAscending();
-		arrayAscending2.printArray();
-		System.out.println("\n" + "Ordenado Insertion Sort");
-		arrayAscending2.insertSort();
-		arrayAscending2.printArray();
-		
-		System.out.println("\n" + "Array Ascending");
-		ArraysTeste arrayAscending3 = new ArraysTeste(tamanhoArray);
-		arrayAscending3.fillArrayAscending();
-		arrayAscending3.printArray();
-		System.out.println("\n" + "Ordenado Selection Sort");
-		arrayAscending3.selectSort();
-		arrayAscending3.printArray();
-		
-		System.out.println("\n" + "Array Descending");
-		ArraysTeste arrayDescending1 = new ArraysTeste(tamanhoArray);
-		arrayDescending1.fillArrayDescending();
-		arrayDescending1.printArray();
-		System.out.println("\n" + "Ordenado Bubble Sort");
-		arrayDescending1.bubbleSort();
-		arrayDescending1.printArray();
-		
-		System.out.println("\n" + "Array Descending");
-		ArraysTeste arrayDescending2 = new ArraysTeste(tamanhoArray);
-		arrayDescending2.fillArrayDescending();
-		arrayDescending2.printArray();
-		System.out.println("\n" + "Ordenado Insertion Sort");
-		arrayDescending2.insertSort();
-		arrayDescending2.printArray();
-		
-		System.out.println("\n" + "Array Descending");
-		ArraysTeste arrayDescending3 = new ArraysTeste(tamanhoArray);
-		arrayDescending3.fillArrayDescending();
-		arrayDescending3.printArray();
-		System.out.println("\n" + "Ordenado Selection Sort");
-		arrayDescending3.selectSort();
-		arrayDescending3.printArray();
+				ArraysTeste arrayShuffle2 = new ArraysTeste(tamanhoArray);
+					
+					System.out.println("\n\t\t" + "Método Insertion Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+							
+							arrayShuffle2.fillArrayAscending();
+							arrayShuffle2.randomizeArray();
+					
+							long startTime = System.currentTimeMillis();
+							arrayShuffle2.insertSort();
+							long endTime = System.currentTimeMillis();
+							
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}
+			
+				ArraysTeste arrayShuffle3 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Selection Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+							
+							arrayShuffle3.fillArrayAscending();
+							arrayShuffle3.randomizeArray();
+					
+							long startTime = System.currentTimeMillis();
+							arrayShuffle3.selectSort();
+							long endTime = System.currentTimeMillis();
+							
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}
+				
+			System.out.println("\n\t" + "Array Ascending");
+			
+				ArraysTeste arrayAscending1 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Bubble Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+									
+							arrayAscending1.fillArrayAscending();
+							
+							long startTime = System.currentTimeMillis();
+							arrayAscending1.bubbleSort();
+							long endTime = System.currentTimeMillis();
+							
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}
+							
+				ArraysTeste arrayAscending2 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Insertion Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+					
+							arrayAscending2.fillArrayAscending();
+							long startTime = System.currentTimeMillis();
+							arrayAscending2.insertSort();
+							long endTime = System.currentTimeMillis();
+							
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}
+			
+				ArraysTeste arrayAscending3 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Selection Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+						
+							arrayAscending3.fillArrayAscending();
+							
+							long startTime = System.currentTimeMillis();
+							arrayAscending3.selectSort();
+							long endTime = System.currentTimeMillis();
+							
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}	
+				
+			
+
+			System.out.println("\n\t" + "Array Descending");
+								
+				ArraysTeste arrayDescending1 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Bubble Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+						
+							arrayDescending1.fillArrayDescending();
+													
+							long startTime = System.currentTimeMillis();
+							arrayDescending1.bubbleSort();
+							long endTime = System.currentTimeMillis();
+							
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");				
+						}
+						
+				ArraysTeste arrayDescending2 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Inserting Sort:");
+					
+						for(int i = 1; i <= quantExecucoes; i++) {
+						
+							arrayDescending2.fillArrayDescending();
+					
+							long startTime = System.currentTimeMillis();
+							arrayDescending2.insertSort();
+							long endTime = System.currentTimeMillis();
+					
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}
+							
+				ArraysTeste arrayDescending3 = new ArraysTeste(tamanhoArray);
+				
+					System.out.println("\n\t\t" + "Método Inserting Sort:");
+						
+						for(int i = 1; i <= quantExecucoes; i++) {
+					
+							arrayDescending3.fillArrayDescending();
+					
+							long startTime = System.currentTimeMillis();
+							arrayDescending3.selectSort();
+							long endTime = System.currentTimeMillis();
+					
+							System.out.println("\t\t\t" + i + "- " + "\t" + (endTime - startTime) + "\t" + "milisegundos");
+						}	
 	}
 }
